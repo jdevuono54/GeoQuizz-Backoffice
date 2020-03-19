dc74c9c9ea6ff4e7ba5e33d45e180723afcb6dfa
habib <benharrat.habib1@gmail.com>
2020-03-19 11:32:05 +0100

    home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>GéoQuizz</h1>
+
+    <!--    <button >-->
+    <!--      <router-link class="button_is_text"  to='/Connexion' >ConnexionC</router-link>-->
+    <!--    </button>-->
+    <!--    <button >-->
+    <!--      <router-link class="button_is_text"  to='/Inscription' >Inscription</router-link>-->
+    <!--    </button>-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+
+}
+</script>
+
+
+<style>
+
+</style>
