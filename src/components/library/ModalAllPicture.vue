f2eaf2bd9a47fecac7dda78c0fb3889987ea8d43
jdevuono <jdevuono>
2020-03-25 16:43:18 +0100

    btn ajout picture

---
+++ src/components/library/ModalAllPicture.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ModalAllPicture"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/library/PictureBox.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "PictureBox"
+    }
+</script>
+
+<style scoped>
+
+</style>
