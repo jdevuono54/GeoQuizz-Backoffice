59e82eb001360bfc074b5788bcf18e31b457e2f5
jdevuono <jdevuono>
2020-03-25 18:37:09 +0100

    remove btn radio & changement place btn cancel

--- src/components/library/ModalAddPicture/FormAddPicture.vue
+++ src/components/library/ModalAddPicture/FormAddPicture.vue
@@ -2,24 +2,15 @@
     <form v-on:submit.prevent="nextStep">
         <div class="form-row">
             <div class="col">
-                <textarea class="form-control" rows="3" placeholder="Description de la photo" required></textarea>
+                <textarea class="form-control" v-model="picture.description" rows="3" placeholder="Description de la photo" required></textarea>
             </div>
         </div>
         <div class="form-row">
             <div class="col">
                 <div><label>Voulez vous ajouter directement la photo à une série éxistante ?</label></div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" v-model="checked" id="radio1" value="true">
-                    <label class="form-check-label" for="radio1">Oui</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" v-model="checked" id="radio2" value="false">
-                    <label class="form-check-label" for="radio2">Non</label>
-                </div>
             </div>
         </div>
         <button type="submit" class="btn btn-primary btn-block">Suivant</button>
-        <b-btn type="button" class="btn btn-danger btn-block" @click="this.$bvModal.show('modal-addpicture')">Annuler</b-btn>
     </form>
 </template>
 
@@ -27,11 +18,6 @@
     export default {
         name: "FormAddPicture",
         props:["picture"],
-        data(){
-            return{
-                checked:null,
-            }
-        },
         methods:{
             nextStep(){
                 return null;
--- src/components/library/ModalAddPicture/ModalAddPicture.vue
+++ src/components/library/ModalAddPicture/ModalAddPicture.vue
@@ -4,7 +4,8 @@
             Ajouter une photo à votre bibliothèque
         </template>
         <div class="d-block text-center">
-            <FormAddPicture></FormAddPicture>
+            <FormAddPicture :picture="picture"></FormAddPicture>
+            <b-btn type="button" class="btn btn-danger btn-block btnCancel" @click="cancel">Annuler</b-btn>
         </div>
     </b-modal>
 </template>
@@ -16,12 +17,32 @@
         components: {FormAddPicture},
         data(){
             return{
-                step:1
+                step:1,
+                picture:{
+                    description:null,
+                    series:{
+                        id_series:null
+                    }
+                }
+            }
+        },
+        methods:{
+            cancel(){
+                this.$bvModal.hide('modal-addpicture');
+                this.step = 1;
+                this.picture = {
+                    description:null,
+                    series:{
+                        id_series:null
+                    }
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .btnCancel{
+        margin-top: 1em;
+    }
 </style>
