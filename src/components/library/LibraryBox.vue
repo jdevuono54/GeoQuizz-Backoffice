c8a7180f931d6de92d8915ffea0f90b0b13117d2
jdevuono <jdevuono>
2020-03-25 16:43:56 +0100

    .

--- src/components/library/LibraryBox.vue
+++ src/components/library/LibraryBox.vue
@@ -1,21 +1,47 @@
 <template>
-    
+    <div class="container-fluid libraryBox">
+        <div class="row text-center rowBtnAddPicture">
+            <button type="button" class="btn btn-primary btnAddPicture col-sm-12 col-md-3 offset-md-9 col-lg-2 offset-lg-10">Ajouter une photo</button>
+        </div>
+        <div class="row">
+            <div class="pictureBox col-sm-12 col-md-3 col-lg-2" v-for="picture in picturesUser">
+                <PictureBox :picture="picture"></PictureBox>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import PictureBox from "./PictureBox";
     export default {
         name: "LibraryBox",
+        components: {PictureBox},
         mounted(){
             this.getAllPicturesUser();
         },
+        data(){
+            return{
+                picturesUser:[]
+            }
+        },
         methods:{
             getAllPicturesUser(){
-                return null
+                this.$axios.get("pictures", {
+                    headers: {Authorization: 'Bearer ' + this.$store.state.user.token}
+                }).then((response) => {
+                    console.log("Récupération des photos réussie")
+                    this.picturesUser = response.data.pictures
+                }).catch(error => {
+                    console.log("Erreur lors de la connexion")
+                    this.$root.makeToast(error.response.data.message)
+                })
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .rowBtnAddPicture{
+        margin: 1em 1em 0 1em;
+    }
 </style>
--- src/components/library/PictureBox.vue
+++ src/components/library/PictureBox.vue
@@ -1,13 +1,32 @@
 <template>
-    $END$
+    <div class="box">
+        <img :src="picture.link">
+        <p>{{ picture.description }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "PictureBox"
+        name: "PictureBox",
+        props:["picture"],
+
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .box{
+        padding: 1em;
+        border: 1px solid black;
+        margin-top: 1em;
+        img{
+            max-width: 100%;
+        }
+        p{
+            margin: 0;
+            text-align: center;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+        }
+    }
 </style>
