8ea928b3bdf19f57881632318ec823b78d3a0cf1
jdevuono <jdevuono>
2020-03-25 01:18:05 +0100

    home ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <div class="container-fluid Home">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 container-home text-center">
+                <b-btn type="button" class="btn btn-block btn-primary">Créer une série</b-btn>
+                <b-btn type="button" class="btn btn-block btn-primary">Modifier une série</b-btn>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +15,19 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .Home {
+        min-height: 100vh;
+        background-image: url("../assets/auth/background_image.svg");
+        background-size: cover;
 
+        .container-home {
+            margin-top: 50vh;
+            transform: translateY(-50%);
+
+            button{
+                background-color: #007bff;
+            }
+        }
+    }
 </style>
