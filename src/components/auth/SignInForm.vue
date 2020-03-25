2651168f33afbe7182723c99e6c11742690ded17
jdevuono <jdevuono>
2020-03-25 01:00:31 +0100

    connexion ok

--- src/components/auth/SignInForm.vue
+++ src/components/auth/SignInForm.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="login-form col-6 offset-3">
         <form v-on:submit.prevent="signIn">
-        <div class="form-group">
-            <input type="email" class="form-control" placeholder="Votre adresse mail" required>
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" placeholder="Votre mot de passe" required>
-        </div>
-        <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+            <div class="form-group">
+                <input type="email" v-model="email" class="form-control" placeholder="Votre adresse mail" required>
+            </div>
+            <div class="form-group">
+                <input type="password" v-model="password" class="form-control" placeholder="Votre mot de passe" required>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Connexion</button>
         </form>
         <router-link to="signup">Pas de compte ? S'inscrire</router-link>
     </div>
@@ -16,16 +16,36 @@
 <script>
     export default {
         name: "SignIn",
-        methods:{
-            signIn(){
-                return null
+        data() {
+            return {
+                email: null,
+                password: null,
+            }
+        },
+        methods: {
+            signIn() {
+                this.$axios.post("user/signin", {
+                    withCredentials: true
+                }, {
+                    auth: {
+                        username: this.email,
+                        password: this.password
+                    }
+                }).then((response) => {
+                    this.$store.commit('saveUser', response.data);
+                    this.$router.push('/Home');
+                    console.log("Connexion réussie")
+                }).catch(error => {
+                    console.log("Erreur lors de la connexion")
+                    this.$root.makeToast(error.response.data)
+                })
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-    a{
+    a {
         font-weight: bold;
         font-size: 1.2em;
     }
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from "vuex-persist";
 
 Vue.use(Vuex)
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key:"backoffice"
+})
+
 export default new Vuex.Store({
+  plugins:[vuexLocal.plugin],
   state: {
+    user:null,
   },
   mutations: {
+    saveUser(state,user){
+      state.user = user;
+    },
+    deleteUser(state){
+      state.user = null
+    }
   },
   actions: {
   },
