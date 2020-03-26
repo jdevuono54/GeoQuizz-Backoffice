58b421e3bf4a60037f722e8f580ea8bf039ee42d
habib <benharrat.habib1@gmail.com>
2020-03-26 14:59:31 +0100

    navbar responsive, reste a limiter l'acces a certeins composents

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
+    <NavBar></NavBar>
     <router-view/>
   </div>
 </template>
+<script>
+  import NavBar from "./components/NavBar";
+  export default {
+    name: "App",
+    components:{
+      NavBar
+    }
 
+  }
+</script>
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
   @import 'node_modules/bootstrap/scss/bootstrap';
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark primary-color bg-info">
+        <b-navbar-brand to="home"><img src="../assets/logo.svg" width="24" alt="logo"></b-navbar-brand>
+
+        <div>
+            <button class="navbar-toggler" type="button" v-on:click="toMenu">Menu</button>
+            <button class="navbar-toggler" type="button"v-on:click="deconnexion">Déconnexion</button>
+        </div>
+
+        <div class="collapse navbar-collapse" id="basicExampleNav">
+            <b-navbar-nav>
+                <b-nav-item  to="home">Menu</b-nav-item>
+                <b-nav-item v-on:click="deconnexion">Déconnexion</b-nav-item>
+            </b-navbar-nav>
+        </div>
+    </nav>
+</template>
+<script>
+    export default {
+        name: "Navbar",
+        methods:{
+            deconnexion(){
+              this.$store.commit("deleteUser")
+              this.$router.push('/signin');
+            },
+            toMenu(){
+                this.$router.push('/home');
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .nav-link {
+        color: white !important;
+        font-weight: bold;
+    }
+    .navbar-collapse{
+        ul{
+            li{
+                text-align: center;
+            }
+        }
+    }
+</style>
